147cff2567161be1eaba315979b45decb80605fb
league <league@192.168.69.12>
2022-12-18 14:32:10 -0800

    goal

--- src/_02_return_types/_1_finish_line/FinishLine.java
+++ src/_02_return_types/_1_finish_line/FinishLine.java
@@ -17,20 +17,22 @@ public class FinishLine {
 	
 	public static void main(String[] args) {
 		//1. Call the drawFinishLine() method
-		
+		drawFinishLine();
 		//2. Call the crazyMove() method to move the robot
-		
+		crazyMove();
 		//3. Call the getTotalDistance() and save what is returned into a variable
-		
+		getTotalDistance();
 		//4. Call the hasCrossedFinishLine() method and save what is returned into a variable
-		
+		hasCrossedFinishLine();
 		//5. If the robot has crossed the finish line... 
-			
+			if(rob.getY() <250) {
 			//6. Use a pop up to say the robot finished and how far it went
-		
-		
+		JOptionPane.showMessageDialog(null, "WINNER");
+			}
 		//7. Else use a pop up to say the robot did not finish and how far it went
-		
+			else {
+				JOptionPane.showMessageDialog(null, "WEINER");
+			}
 	}
 	
 	static void crazyMove() {
--- src/_02_return_types/_2_list_of_goals/ListOfGoals.java
+++ src/_02_return_types/_2_list_of_goals/ListOfGoals.java
@@ -10,13 +10,15 @@ public class ListOfGoals {
 	
 	public static void main(String[] args) {
 		//1. Call the appropriate method to make your list of goals
-		
+		createListOfGoals();
 		//2. Call the appropriate method to get your list of goals, 
 		//   so you can use a pop-up to display them
+		String goal=getListOfGoals();
+		JOptionPane.showMessageDialog(null, goal);
 		
 		//3. Call the appropriate method to get the number of goals you entered,
 		//   and using a pop-up display the number of goals
-		
+		numberOfGoals();
 		//4. Call the appropriate method to see if you put a lot of goals,
 		//   if you did put a lot of goals use a pop-up to say "Wow that's a lot of goals! Awesome!"
 		
